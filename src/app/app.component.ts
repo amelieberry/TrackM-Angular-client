d4019dea3b58372f693cdf5489d0d601d523d578
Amelie <aac.berry@gmail.com>
2023-01-26 13:38:45 +0100

    implemented: open registration dialog function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrackM-Angular-client';
+
+  constructor(public dialog: MatDialog) {}
+
+  //When the sign-up button is clicked, open registration dialog
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
 }
