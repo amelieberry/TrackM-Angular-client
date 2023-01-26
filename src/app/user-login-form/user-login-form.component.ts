59f023befdb219118fdcfd4fa8ccb5ee60a266fe
Amelie <aac.berry@gmail.com>
2023-01-26 13:46:34 +0100

    generate: user login form component

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent {
+
+}
