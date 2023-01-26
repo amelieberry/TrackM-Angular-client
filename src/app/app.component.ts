ca3a97edab04123a799c01d40ff47eccfc3df89c
Amelie <aac.berry@gmail.com>
2023-01-26 16:11:34 +0100

    implement: openUserLoginDialog function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -15,6 +16,13 @@ export class AppComponent {
   //When the sign-up button is clicked, open registration dialog
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+
+  //When the Login button is clicked, open login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     });
   }
