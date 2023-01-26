3efd543c149376c1578e2962f19ff9fa74688d4b
Amelie <aac.berry@gmail.com>
2023-01-26 16:13:44 +0100

    define input/output, implement loginUser()

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// Angular Material Imports
+import { MatDialogRef } from '@angular/material/dialog'; // to close dialog on success
+import { MatSnackBar } from '@angular/material/snack-bar'; // to display notifications back to user
+
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-login-form',
@@ -6,5 +12,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent {
+
+  @Input() userData = {
+    Username: '',
+    Password: '',
+  };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  // called once the component has received all its inputs from the calling component
+  ngOnInit(): void {
+
+  }
+
+  // send the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      this.dialogRef.close();
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
 
 }
