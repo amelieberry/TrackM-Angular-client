23a24148a507a547b102475f82d5f80da08d4bfd
Amelie <aac.berry@gmail.com>
2023-01-30 12:59:51 +0100

    moved dialog functions to welcome-page.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +6,5 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TrackM-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-
-  //When the sign-up button is clicked, open registration dialog
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
-    });
-  }
-
-  //When the Login button is clicked, open login dialog
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
+  title = 'TrackM-Angular-client';  
 }
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void { }
+  
+  //When the sign-up button is clicked, open registration dialog
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+
+  //When the Login button is clicked, open login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
 
 }
