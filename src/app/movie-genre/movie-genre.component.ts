bbe1c9da5131b7dd627f90bc2873834bd5f69eca
Amelie <aac.berry@gmail.com>
2023-01-31 13:53:24 +0100

    Inject dialog data into movie genre component

---
+++ src/app/movie-genre/movie-genre.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-genre',
+  templateUrl: './movie-genre.component.html',
+  styleUrls: ['./movie-genre.component.scss']
+})
+export class MovieGenreComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) 
+      public data: {
+        Name: string;
+        Description: string;
+      }
+  ) {}
+}
