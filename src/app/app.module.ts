3166a57a8d8e5de828a6c7e11321a397af453dec
Amelie <aac.berry@gmail.com>
2023-01-31 13:56:59 +0100

    generate movie-director component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieGenreComponent } from './movie-genre/movie-genre.component';
+import { MovieDirectorComponent } from './movie-director/movie-director.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MovieGenreComponent } from './movie-genre/movie-genre.component';
     UserLoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
-    MovieGenreComponent
+    MovieGenreComponent,
+    MovieDirectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-director/movie-director.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieDirectorComponent } from './movie-director.component';
+
+describe('MovieDirectorComponent', () => {
+  let component: MovieDirectorComponent;
+  let fixture: ComponentFixture<MovieDirectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MovieDirectorComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MovieDirectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
