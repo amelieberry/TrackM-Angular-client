347aa1d4127e64ec4e6d6b8dd497bba791bb6093
Amelie <aac.berry@gmail.com>
2023-02-01 16:20:02 +0100

    router.navigate to movies, profile, welcome

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
+  constructor(
+    private router: Router
+  ) {}
 
+  /**
+   * navigate the user to the movies page on click
+   */
+  navigateMovies() {
+    this.router.navigate(['movies']);
+  }
+
+  /**
+   * navigate the user to the profile page on click
+   */
+  navigateProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  /**
+   * navigate the user to the welcome page on click and clear localStorage
+   */
+  logoutUser(): void {
+    this.router.navigate(['welcome']);
+    localStorage.clear();
+  }
 }
