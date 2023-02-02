ca5b36db1d8d410a9a144fdb3a516aa571db36f6
Amelie <aac.berry@gmail.com>
2023-02-02 17:45:23 +0100

    getUserInfo gets user oninit, stores it

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any = {};
 
+  @Input() updatedUser = {
+    Username: '',
+    Password: '',
+    Email: '',
+    Birthday: ''
+  }
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = {
+        ...resp     
+      }
+      return this.user;
+    })
+  }
 }
