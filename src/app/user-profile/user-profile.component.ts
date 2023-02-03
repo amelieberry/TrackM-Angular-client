f08e9962e6038b0aac2888222cad0ee1b9797b7d
Amelie <aac.berry@gmail.com>
2023-02-03 14:23:41 +0100

    updateUserInfo: updates user info on click

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { FetchApiDataService } from '../fetch-api-data.service';
 
@@ -17,21 +18,61 @@ export class UserProfileComponent implements OnInit {
     Email: '',
     Birthday: ''
   }
+ 
 
   constructor(
     public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    private router: Router
+
   ) {}
 
   ngOnInit(): void {
     this.getUserInfo();
   }
 
+  /**
+   * Get user data from API, store value in "user"
+   * @function getUserInfo
+   */
   getUserInfo() {
     this.fetchApiData.getUser().subscribe((resp: any) => {
       this.user = {
-        ...resp     
+        ...resp,
+        Birthday: new Date(resp.Birthday).toLocaleDateString()
       }
+      console.log(this.user);
       return this.user;
     })
   }
+
+  /**
+   * 
+   */
+  updateUserInfo() {
+    console.log('updateUserInfo',this.updatedUser);
+
+    this.updatedUser = {
+      Username: this.updatedUser.Username === "" || null ? this.user.Username : this.updatedUser.Username,
+      Password: this.updatedUser.Password === "" || null ? this.user.Password : this.updatedUser.Password,
+      Email: this.updatedUser.Email === "" || null ? this.user.Email : this.updatedUser.Email,
+      Birthday: this.updatedUser.Birthday === "" || null ? this.user.Birthday : this.updatedUser.Birthday,
+    }  
+
+    this.fetchApiData.updateUser(this.updatedUser).subscribe((result) => {
+      console.log(result);
+      localStorage.setItem('username', result.Username);
+      this.snackBar.open('Profile Updated', 'OK', {
+        duration: 5000
+      });
+      window.location.reload();
+    }, (result) => {
+      console.log(result)
+    });
+  }
+
+  // delete user
+  // deleteUserInfo() {
+
+  // }
 }
