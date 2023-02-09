70c5c6367cd46e0f4569f3bead26fd6b71dfadc2
Amelie <aac.berry@gmail.com>
2023-02-09 15:57:09 +0100

    inject apinner service into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerService } from './loading-spinner.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrackM-Angular-client';  
+
+  constructor( public spinnerService: LoadingSpinnerService) {}
 }
